91821cfb8a23aa23007bcc9dcf94c7a267153035
julbautista <julbautista@yahoo.com>
2016-12-13 00:34:04 -0500

    create data for number 4
    creates a data set for question 4 and calls it in final_exam

---
+++ 4_data.R
@@ -0,0 +1,43 @@
+setwd("C:/Users/Julian Bautista/Documents/School Stuff/Semesters/4 Fall 2016/Applied Statistics III/Final Exam")
+library(readstata13)
+library(dplyr)
+
+#pulling the data from web
+pew <- read.dta13('http://www.stat.columbia.edu/~gelman/bda.course/pew_research_center_june_elect_wknd_data.dta')
+elect <- read.csv("http://www.stat.columbia.edu/~gelman/bda.course/2008ElectionResult.csv")
+
+#combining party affiliation from heat2 and heat 4 with corresponding marital
+pop <- data.frame(pew$state, pew$heat2, pew$marital)
+pop2 <- data.frame(pew$state,pew$heat4, pew$marital)
+pop <- pop[complete.cases(pop$pew.heat2),]
+pop2 <- pop2[complete.cases(pop2$pew.heat4),]
+names(pop) <- c("state", "party", "marital")
+names(pop2) <- c("state", "party", "marital")
+marriage <- rbind(pop,pop2)
+
+#drop third parties
+marriage <- filter(marriage, party == "rep/lean rep" | party == "dem/lean dem")
+
+#drop hawaii and  alaska
+marriage <- filter(marriage, state != "hawaii" & state != "alaska")
+elect <- filter(elect, state != "Hawaii" & state != "Alaska")
+
+#0 if republican, 1 if democrat; 0 if not married, 1 if married
+marriage$party <- ifelse(marriage$party == "rep/lean rep", 0,1)
+marriage$marital <- ifelse(marriage$marital == "married", 1,0)
+
+#remove dropped state factors then generate state id
+marriage$state <- factor(marriage$state)
+marriage$id <- as.numeric(marriage$state)
+
+#removes index 
+#rowname(marriage) <- NULL
+
+#removing excess data.frames
+rm("pop","pop2","pew")
+
+#creating variables
+state_id <- marriage$id
+party <- marriage$party
+marital <- marriage$marital
+state <- levels(marriage$state)
---
+++ Final_Exam.R
@@ -0,0 +1,8 @@
+setwd("C:/Users/Julian Bautista/Documents/School Stuff/Semesters/4 Fall 2016/Applied Statistics III/Final Exam")
+
+
+#number 3 simulation
+
+
+#number 4
+source("4_data.r")
